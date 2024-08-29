ff03bcde037f7fe855e0a86d3811860d57592313
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-08-29 08:35:33 -0300

    fix: :bug: Fix download directory not working.

--- bots/bot_receitas.py
+++ bots/bot_receitas.py
@@ -6,14 +6,13 @@ from time import sleep
 import requests
 
 
-
 class RevenueBot:
     """
     Bot para baixar os dados de receitas do Portal da Transparência.
     """
 
     def __init__(self, web_url: str, download_time: int):
-        self.donwload_folder = os.getcwd() + "/csv"
+        self.donwload_folder = f"{os.getcwd()}/csv/"
         self.url = web_url
         self.download_wait_time = download_time
         
@@ -33,7 +32,9 @@ class RevenueBot:
             headers = {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
             }
+            
             response = requests.get(web_url, headers=headers)
+            
             if response.status_code == 200:
                 return "Status da Página: Online"
             else:
@@ -43,7 +44,7 @@ class RevenueBot:
 
     def clear_folder(self):
         """
-        Limpa a pasta de downloads se ela não estiver vazia.
+        Limpa a pasta de downloads (csv) se ela não estiver vazia.
         """
         if len(os.listdir(self.donwload_folder)) > 0:
             for file in os.listdir(self.donwload_folder):
@@ -57,13 +58,11 @@ class RevenueBot:
         desp_receitas = self.driver.find_element(By.ID, 'despesas-card')
         desp_receitas.click()
 
-        receitas_consulta = self.driver.find_element(
-            By.CSS_SELECTOR, '#receitas-links')
+        receitas_consulta = self.driver.find_element(By.CSS_SELECTOR, '#receitas-links')
         consultas_link = receitas_consulta.find_elements(By.TAG_NAME, 'a')[1]
         consultas_link.click()
 
-        tabela_opcoes = self.driver.find_element(
-            By.CLASS_NAME, 'box-tabela-completa__opcoes')
+        tabela_opcoes = self.driver.find_element(By.CLASS_NAME, 'box-tabela-completa__opcoes')
         baixar_link = tabela_opcoes.find_elements(By.TAG_NAME, 'a')[1]
         baixar_link.click()
 
