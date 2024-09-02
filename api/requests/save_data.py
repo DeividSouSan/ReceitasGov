eba7db20a0663f2cd9558fbad701c8858f9e0345
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-09-02 06:09:17 -0300

    refactor: :truck: Rename api related function.

--- api/requests/save_data.py
+++ api/requests/save_data.py
@@ -5,12 +5,11 @@ import os
 import requests
 
 
-def post_to_api(url: str) -> None:
+def save_data(url: str) -> None:
     base_path = os.getcwd()
 
     with open(f"{base_path}/output/output.json", "r", encoding="utf-8") as file:
         data_json = json.loads(file.read())
 
     response = requests.post(url, json=data_json)
-
     logging.info(f"Status da Requisição: {response.status_code}")
