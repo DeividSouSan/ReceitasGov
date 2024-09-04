7672eb3be377823d753d98a3b5c0d0df53aead62
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-09-04 06:10:36 -0300

    feat: :sparkles: Import frames and adds to window.

--- source/main.py
+++ source/main.py
@@ -2,21 +2,24 @@ import logging
 import os
 from datetime import datetime
 
+from gui.frames.config_page import ConfigPage
+from gui.frames.main_page import MainPage
+from gui.frames.read_files_page import ReadFilePage
 from gui.window import Window
 
 # Capturando o diretório atual
 path = os.getcwd()
 
 # Configurando o log
-log_filename = (
+logs_path = (
     f"{path}/source/logs/app_usage_{datetime.now().strftime('%d%m%Y_%H_%M_%S')}.log"
 )
-logging.basicConfig(filename=log_filename, level=logging.INFO)
+logging.basicConfig(filename=logs_path, level=logging.INFO)
 
 logging.info(
     f"Iniciando o programa: {datetime.now().strftime('%d/%m/%Y, %H:%M:%S')}..."
 )
 
 # Iniciando a janela principal
-root = Window()
+root = Window(frames=[MainPage, ConfigPage, ReadFilePage])
 root.mainloop()
