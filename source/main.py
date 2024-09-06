8bf4538bd3d734e645ce299ad4dfb858ebc658bb
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-09-06 06:50:08 -0300

    refactor: :art: Add frames into Window constructor.

--- source/main.py
+++ source/main.py
@@ -7,18 +7,10 @@ from gui.frames.main_page import MainPage
 from gui.frames.read_files_page import ReadFilePage
 from gui.window import Window
 
-# Capturando o diretório atual
-path = os.getcwd()
-
-# Configurando o log
-logs_path = (
-    f"{path}/source/logs/app_usage_{datetime.now().strftime('%d%m%Y_%H_%M_%S')}.log"
-)
-logging.basicConfig(filename=logs_path, level=logging.INFO)
-
-logging.info(
-    f"Iniciando o programa: {datetime.now().strftime('%d/%m/%Y, %H:%M:%S')}..."
-)
+# Configurando o Log
+log_path = os.path.join(os.getcwd(), "source/logs")
+log_name = f"app_usage_{datetime.now().strftime('%d%m%Y_%H_%M_%S')}.log"
+logging.basicConfig(filename=f"{log_path}/{log_name}", level=logging.INFO)
 
 # Iniciando a janela principal
 root = Window(frames=[MainPage, ConfigPage, ReadFilePage])
