1efdb81de3a08958cec15cae19e66db00a051ca3
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-09-23 06:02:50 -0300

    refactor: :recycle: Removes inline foreground-color.

--- source/gui/frames/base_page.py
+++ source/gui/frames/base_page.py
@@ -3,4 +3,4 @@ import customtkinter as ctk
 
 class BasePage(ctk.CTkFrame):
     def __init__(self, parent: ctk.CTkFrame):
-        ctk.CTkFrame.__init__(self, parent, fg_color="#23272a")
+        ctk.CTkFrame.__init__(self, parent)
