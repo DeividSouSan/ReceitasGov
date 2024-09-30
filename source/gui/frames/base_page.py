ae5b2b57966d1e8d476f070b7aab8094400c3ea4
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-09-30 06:20:23 -0300

    refactor: :recycle: BasePage now contains controller instance.
    Before, the controller instance was used in the page class, not the BasePage. As it's used in every page I thought it made sense to make it only present inside the BasePage.

--- source/gui/frames/base_page.py
+++ source/gui/frames/base_page.py
@@ -2,5 +2,7 @@ import customtkinter as ctk
 
 
 class BasePage(ctk.CTkFrame):
-    def __init__(self, parent: ctk.CTkFrame):
+    def __init__(self, parent: ctk.CTkFrame, controller: ctk.CTk):
         ctk.CTkFrame.__init__(self, parent)
+
+        self.controller = controller
